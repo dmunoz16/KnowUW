32446b8b14462686baf7a532c224af2538af4461
Diana Muñoz <diana16_djms@live.com>
2020-03-10 03:44:10 -0700

    Added Preloader code and fixed footer issues. Updated About Us

--- script.js
+++ script.js
@@ -67,3 +67,23 @@ $('.slider').each(function() {              // For every slider
   
   
   });
+
+  //Preloader
+  $(document).ready(function() {
+    setTimeout(function() {
+      $('.wrapper').addClass('loaded');
+      
+    }, 3000);
+  });
+  
+  
+  
+  jQuery(function(){
+  
+    $(window).load(function(){
+    
+    $('.wrapper').removeClass('preload');
+    
+    });
+  
+  });
